9e0b0645d4b011a177f38b2a354ae5eb5b37d69f
Michael Sarahan <msarahan@nvidia.com>
2024-11-13 22:00:24 -0600

    Simplify and document dispatch action strategy

--- README.md
+++ README.md
@@ -1,33 +1,35 @@
 # shared-actions
 
-Contains all of the shared composite actions used by RAPIDS.
+Contains all of the shared composite actions used by RAPIDS. Several of these actions,
+especially the telemetry actions, use a pattern that we refer to as "dispatch actions."
+The general idea of a dispatch action is to make it easier to depend on other actions
+at a specific revision, and also to simplify using files beyond a given action .yml file.
+
+A dispatch action is one that:
+* clones the shared-actions repository (repo/ref changeable using env vars)
+* runs (dispatches to) another action within the clone, using a relative path
+
+There can be more complicated arrangements of more actions, but the idea is to
+have the local clone of the shared-actions repository be the first step of an action.
 
 Actions that refer to each other assume that they have been checked out to the
 ./shared-actions folder. This *should* be the root of the GitHub Actions workspace.
 This assumption is what allow code reuse between actions.
 
-In general, we should try to never call "implementation actions" here. Instead,
-we should prefer to create "dispatch actions" that clone shared-actions from a particular repo
-at a particular ref, and then dispatch to an implementation action from that repo.
-This adds complexity, but has other advantages:
-
-* simplifies specifying a custom branch for actions for development and testing
-* changes all shared-actions calls in a workflow at once, instead of changing each one
-* allows reuse of shared-actions within the shared-actions repo. Trying to use these
-  without the clone and relative path would not otherwise keep the repo and ref
-  consistent, leading to great confusion over why changes aren't being reflected.
+Actions that use this pattern should include "dispatch" in their folder name, so
+that they can be readily distinguished from any actions that are either
+standalone or otherwise implementations that assume that the ./shared-actions
+folder is already cloned, so that they can use relative paths to reference other
+actions and files.
 
 ## Example dispatch action
 
 ```yaml
-name: 'Example dispatch action'
+name: 'dispatch-example-action'
 description: |
   The purpose of this wrapper is to keep it easy for external consumers to switch branches of
   the shared-actions repo when they are changing something about shared-actions and need to test it
   in their pipelines.
-
-  Inputs here are all assumed to be env vars set outside of this script.
-  Set them in your main repo's workflows.
 
 runs:
   using: 'composite'
@@ -38,28 +40,38 @@ runs:
         repository: ${{ env.SHARED_ACTIONS_REPO}}
         ref: ${{ env.SHARED_ACTIONS_REF}}
         path: ./shared-actions
-    - name: Stash base env vars
-      uses: ./shared-actions/_stash-base-env-vars
+    - name: Run local implementation action
+      uses: ./shared-actions/impls/example-action
 ```
 
 In this action, the "implementation action" is the
-`./shared-actions/_stash-base-env-vars`.  You can have inputs in your
+`./shared-actions/impls/example-action`.  You can have inputs in your
 dispatch actions. You would just pass them through to the implementation action.
 Environment variables do carry through from the parent workflow through the
-dispatch action, into the implemetation action. In most cases, it is simpler
+dispatch action, and then into the implemetation action. In most cases, it is simpler
 (though less explicit) to set environment variables instead of plumbing inputs
 through each action.
-
-Environment variables are hard-coded, not detected. If you want to pass a different
-environment variable through, you need to add it to implementation stash action,
-like `telemetry-impls/stash-base-env-vars/action.yml`. You do not need to
-explicitly specify it on the loading side.
 
 ## Implementation action
 
 These are similar to dispatch actions, except that they should not clone
 shared-actions. They can depend on other actions from the shared-actions
 repository using the `./shared-actions` relative path.
+
+```yaml
+name: 'example-action'
+description: |
+  An example of calling a python script in an action
+
+runs:
+  using: 'composite'
+  steps:
+    - name: Run local action
+      uses: ./shared-actions/impls/another-action
+    - name: Run local script file
+      run: python -c "./shared-actions/impls/hello.py"
+      shell: bash
+```
 
 ## Example calling workflow
 
@@ -77,8 +89,74 @@ jobs:
   actions-user:
     runs-on: ubuntu-latest
     steps:
-      - name: Telemetry setup
-        id: telemetry-setup
+      - name: Call dispatch example
         # DO NOT change this in PRs
-        uses: rapidsai/shared-actions/dispatch-script@main
+        uses: rapidsai/shared-actions/dispatch-example-action@main
 ```
+
+This works because the environment variables get passed into the shared action. They are then
+used by the `actions/checkout` action, taking priority over the default values.
+
+## Calling in child shared workflows
+
+Shared workflows complicate matters because environment variables do not get
+passed through. If you set the `SHARED_ACTIONS_REPO` and/or `SHARED_ACTIONS_REF`
+variables in the top-level parent workflow, they will not take effect in any
+dispatch actions that you may call in child workflows. You can pass them as inputs
+to child shared workflows, but that ends up being very verbose.
+
+To carry this information into child workflows, we use a scheme that writes a
+file with environment variables, uploads this file as an artifact, then downloads
+and loads the file at the start of the child workflow.
+
+The general scheme is:
+
+### Top-level workflow
+```yaml
+jobs:
+  setup-env-vars:
+    runs-on: ubuntu-latest
+    steps:
+        # implicitly picks up env vars for SHARED_ACTIONS_REPO and _REF
+        - uses: rapidsai/shared-actions/telemetry-dispatch-stash-base-env@main
+
+  <rest of jobs>
+
+  summarize-telemetry:
+    needs: <all other jobs, or just pr-builder>
+    # private networks will affect your choice here. If your tempo server or
+    # forwarder/collector is only accessible on some instances, then use one of
+    # those instances here
+    runs-on: <node>
+    steps:
+      - uses: rapidsai/shared-actions/telemetry-dispatch-summarize@main
+        # if you use mTLS, this is probably the right place to pass in the certificates
+```
+
+### Child workflows
+```yaml
+jobs:
+    tests:
+    strategy:
+      matrix: ${{ fromJSON(needs.compute-matrix.outputs.MATRIX) }}
+    runs-on: "linux-${{ matrix.ARCH }}-gpu-${{ matrix.GPU }}-${{ matrix.DRIVER }}-1"
+    steps:
+      - name: Telemetry setup
+        uses: rapidsai/shared-actions/telemetry-dispatch-setup@main
+        continue-on-error: true
+
+      <other steps, as usual>
+
+      - name: Stash metadata for this job (matrix values)
+        uses: rapidsai/shared-actions/telemetry-dispatch-stash-job-attributes@main
+        continue-on-error: true
+        if: always()
+        with:
+          extra_attributes: "rapids.cuda=${{matrix.CUDA_VER}},rapids.py=${{matrix.PY_VER}},rapids.gpu=${{matrix.GPU}}"
+```
+
+Behind the scenes, the implementation actions are:
+* ./telemetry-impls/stash-base-env-vars: storing base environment variables (including setting default values):
+* ./telemetry-impls/load-then-clone: Downloads base env var file, loads it, then
+  clones shared-actions according to env vars that were just loaded
+* ./telemetry-impls/summarize: Runs Python script to parse GitHub logs and send OpenTelemetry spans to endpoint
---
+++ telemetry-impls/summarize/send_trace.py
@@ -0,0 +1,308 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+"""This is a Sample Python file."""
+
+
+from __future__ import annotations
+from datetime import datetime
+from functools import partial
+import hashlib
+from pathlib import Path
+import json
+import time
+import os
+import re
+
+from enum import Enum
+from typing import Optional, Mapping, Union, Iterable
+from opentelemetry import trace
+from opentelemetry.sdk.trace import Span
+from opentelemetry.trace import SpanKind
+from opentelemetry.context import Context
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.resources import Resource
+from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.trace.status import Status, StatusCode
+
+match os.getenv("OTEL_EXPORTER_OTLP_PROTOCOL"):
+    case "http/protobuf":
+        from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+    case "grpc":
+        from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+    case _:
+        from opentelemetry.sdk.trace.export import ConsoleSpanExporter as OTLPSpanExporter
+
+
+try:
+    from typing import Literal
+except ImportError:  # pragma: no cover
+    from typing_extensions import Literal
+
+__version__ = "0.0.1"
+
+
+def strtobool(val: str) -> bool:
+    """
+    Convert a string representation of truth to true (1) or false (0).
+
+    True values are y, yes, t, true, on and 1; false values are n, no, f,
+    false, off and 0. Raises ValueError if val is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return False
+    else:
+        raise ValueError(f"Unable to parse '{val!r}' as boolean")
+
+
+_attribute_casting = {
+    "int": int,
+    "float": float,
+    "bool": strtobool,
+    "str": str,
+}
+
+
+def parse_attributes(attrs: Iterable[str]) -> Mapping[str, Union[str, int, list, bool]]:
+    """
+    Attempt to parse attributes in a given list `attrs`.
+    Special handling is done for attributes which begin with these prefixes:
+        'str:' -> Value will be converted to string using str() (default)
+        'int:' -> Value will be converted to integer using int()
+        'float:' -> Value will be converted to float using float()
+        'bool:' -> Value will be converted to bool.
+                   True values are 'y', 'yes', 't', 'true', 'on', and '1'
+                   False values are 'n', 'no', 'f', 'false', 'off', and '0'.
+
+    In order to pass multiple values, add "[]" to the prefix like so:
+        'int[]:my-array=1,2,3,4'
+
+    You can customize the separator like so:
+        'int[sep=;]:my-array=1;2;3;4'
+        'str[sep=:]:path-array=/usr/bin:/bin'
+    """
+    attr_pattern = re.compile(r"^(?:(?P<prefix>.*):)?(?P<name>[^=]*)=(?P<value>.*)$")
+    prefix_pattern = re.compile(r"^(?P<type>[^\[\n]*)(?P<array>\[(sep=?(?P<sep>.))?.*\])?$")
+    attributes = {}
+    for attr in attrs:
+        attr_match = attr_pattern.match(attr)
+        if attr_match is None:
+            raise ValueError(f"Unable to parse attribute: {attr}")
+
+        prefix, key, value = attr_match.group("prefix", "name", "value")
+
+        if prefix is None:
+            attributes[key] = value
+            continue
+
+        prefix_match = prefix_pattern.match(prefix)
+        if prefix_match:
+            prefix_type = prefix_match.group("type")
+            cast_function = _attribute_casting.get(prefix_type, lambda x: x)
+
+            if prefix_match.group("array") is None:
+                attributes[key] = cast_function(value)
+                continue
+
+            value_separator = prefix_match.group("sep") or ","
+            attributes[key] = tuple([cast_function(item) for item in value.split(value_separator)])
+
+    return attributes
+
+
+def parse_attribute_file(filename: str) -> Mapping[str, Union[str, int, list, bool]]:
+    with open(filename, "r") as attribute_file:
+        attributes = parse_attributes(attribute_file.readlines())
+    return attributes
+
+
+def create_span(
+    span_name: str,
+    service_name: str = "otel-cli-python",
+    service_version: str = __version__,
+    start_time: Optional[int] = None,
+    end_time: Optional[int] = None,
+    trace_id: Optional[str] = None,
+    span_id: Optional[str] = None,
+    kind: Literal["client", "consumer", "internal", "producer", "server"] = "internal",
+    traceparent: Optional[str] = None,
+    attributes: Optional[Mapping[str, str]] = None,
+    status_code: Literal["UNSET", "OK", "ERROR"] = "UNSET",
+    status_message: Optional[str] = None,
+) -> Span:
+    resource = Resource.create(
+        attributes={
+            "service.name": service_name,
+            "service.version": service_version,
+        }
+    )
+    provider = TracerProvider(resource=resource)
+    otlp_exporter = OTLPSpanExporter()
+    provider.add_span_processor(BatchSpanProcessor(otlp_exporter))
+    tracer = trace.get_tracer("otel-cli-python", __version__, tracer_provider=provider)
+
+    if trace_id is not None:
+        tracer.id_generator.generate_trace_id = lambda: int(trace_id, 16)
+
+    if span_id is not None:
+        tracer.id_generator.generate_span_id = lambda: int(span_id, 16)
+
+    if start_time is None:
+        start_time = time.time_ns()
+
+    if end_time is None:
+        end_time = time.time_ns()
+
+    # Create a new context to avoid reusing context created by pytest
+    context = Context()
+    if not traceparent:
+        traceparent = os.getenv("TRACEPARENT")
+    if traceparent is not None:
+        carrier = {"traceparent": traceparent}
+        context = TraceContextTextMapPropagator().extract(carrier)
+
+    span_kind = SpanKind[kind.upper()]
+    statuscode = StatusCode[status_code]
+    span_status = Status(statuscode, description=status_message)
+    my_span = tracer.start_span(
+        span_name,
+        start_time=start_time,
+        kind=span_kind,
+        context=context,
+        attributes=attributes,
+    )
+    my_span._status = span_status
+    my_span.end(end_time=end_time)
+    return my_span
+
+
+def trace_id(run_url: str, run_attempt: str):
+    trace_id = hashlib.sha256()
+    trace_id.update(f"{run_url}+{run_attempt}".encode())
+    return trace_id.hexdigest()[:32]
+
+
+def span_id(trace_id: str, job_name: str, step_name: Optional[str] = None):
+    span_id = hashlib.sha256()
+    span_id.update(trace_id.encode())
+    span_id.update(bytes(job_name.encode()))
+    if step_name:
+        span_id.update(bytes(step_name.encode()))
+    return span_id.hexdigest()[:16]
+
+
+def traceparent(run_url, run_attempt, job_name, step_name: Optional[str] = None):
+    tid = trace_id(run_url=run_url, run_attempt=run_attempt)
+    sid = span_id(trace_id=tid, job_name=job_name, step_name=step_name)
+    return f"00-{tid}-{sid}-01"
+
+
+def date_str_to_epoch(date_str: str, value_if_not_set: int):
+    if date_str:
+        timestamp_ns = int(datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%SZ").timestamp() * 1e9)
+    else:
+        timestamp_ns = value_if_not_set
+    return timestamp_ns
+
+
+def map_conclusion_to_status_code(conclusion: str):
+    if conclusion == "success":
+        return "OK"
+    elif conclusion == "failure":
+        return "ERROR"
+    else:
+        return "UNSET"
+
+
+def main(args):
+    with open("all_jobs.json") as f:
+        jobs = json.loads(f.read())
+    tid = trace_id(jobs[0]["run_url"], jobs[0]["run_attempt"])
+    try:
+        top_level_job_name = os.environ["OTEL_SERVICE_NAME"]
+    except KeyError:
+        print("The `OTEL_SERVICE_NAME` environment variable must be set. Exiting")
+        sys.exit(1)
+    top_level_span_id = span_id(tid, top_level_job_name)
+    top_level_traceparent = f"00-{tid}-{top_level_span_id}-01"
+    # track the latest timestamp observed and use it for any unavailable times.
+    last_timestamp = date_str_to_epoch(jobs[0]["completed_at"], 0)
+    for job in jobs:
+        job_name = job["name"]
+        job_id = job["id"]
+        job_span_id = span_id(tid, job_name=job["name"])
+        job_traceparent = f"00-{tid}-{job_span_id}-01"
+
+        attribute_file = Path.cwd() / f"telemetry-tools-attrs-${job_id}/attrs-${job_id}"
+        if attribute_file.exists():
+            attributes = parse_attribute_file(attribute_file.as_posix())
+        elif attributes_env_var := os.getenv("OTEL_RESOURCE_ATTRIBUTES"):
+            attributes = parse_attributes(attributes_env_var.split(","))
+        else:
+            attributes = None
+
+        for step in job["steps"]:
+            step_span_id = span_id(trace_id=tid, job_name=job_name, step_name=step["name"])
+            start = date_str_to_epoch(step["started_at"], last_timestamp)
+            end = date_str_to_epoch(step["completed_at"], last_timestamp)
+
+            if end > last_timestamp:
+                last_timestamp = end
+            create_span(
+                span_name=step["name"],
+                service_name=job_name,
+                trace_id=tid,
+                span_id=step_span_id,
+                traceparent=job_traceparent,
+                start_time=start,
+                end_time=end,
+                attributes=attributes,
+                status_code=map_conclusion_to_status_code(step["conclusion"]),
+            )
+        job_create = date_str_to_epoch(job["created_at"], last_timestamp)
+        job_start = date_str_to_epoch(job["started_at"], last_timestamp)
+        job_end = max(date_str_to_epoch(job["completed_at"], last_timestamp), last_timestamp)
+
+        if job_name == top_level_job_name:
+            create_span(
+                span_name="workflow root",
+                service_name=job_name,
+                trace_id=tid,
+                span_id=job_span_id,
+                start_time=job_create,
+                end_time=job_end,
+                attributes=attributes,
+                status_code=map_conclusion_to_status_code(job["conclusion"]),
+            )
+        else:
+            create_span(
+                span_name="Start delay time",
+                service_name=job_name,
+                trace_id=tid,
+                traceparent=job_traceparent,
+                start_time=job_create,
+                end_time=job_start,
+                attributes=attributes,
+            )
+            create_span(
+                span_name="child workflow root",
+                service_name=job_name,
+                trace_id=tid,
+                span_id=job_span_id,
+                traceparent=top_level_traceparent,
+                start_time=job_create,
+                end_time=job_start,
+                attributes=attributes,
+                status_code=map_conclusion_to_status_code(job["conclusion"]),
+            )
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv[1:])

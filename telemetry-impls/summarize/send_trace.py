0da6147cf5f9d4fc0f889d2eb28bdb22005d6874
Michael Sarahan <msarahan@nvidia.com>
2024-12-06 12:15:17 -0600

    clean up python script

--- telemetry-impls/summarize/send_trace.py
+++ telemetry-impls/summarize/send_trace.py
@@ -1,13 +1,21 @@
-#   ---------------------------------------------------------------------------------
-#   Copyright (c) Microsoft Corporation. All rights reserved.
-#   Licensed under the MIT License. See LICENSE in project root for information.
-#   ---------------------------------------------------------------------------------
-"""This is a Sample Python file."""
+# Copyright (c) 2019-2024, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Processes a GitHub Actions workflow log record and outputs OpenTelemetry span data."""
 
 
 from __future__ import annotations
 from datetime import datetime
-from functools import partial
 import hashlib
 from pathlib import Path
 import json
@@ -15,7 +23,6 @@ import time
 import os
 import re
 
-from enum import Enum
 from typing import Optional, Mapping, Union, Iterable
 from opentelemetry import trace
 from opentelemetry.sdk.trace import Span

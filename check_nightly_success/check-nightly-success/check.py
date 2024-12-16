a7ac6b5c647eaf169625c2a06bd62153c184a92b
Vyas Ramasubramani <vyasr@nvidia.com>
2024-12-16 19:19:40 +0000

    Address PR comments

--- check_nightly_success/check-nightly-success/check.py
+++ check_nightly_success/check-nightly-success/check.py
@@ -1,13 +1,17 @@
-import requests
-import itertools
-from datetime import datetime
-import sys
+"""Check whether a GHA workflow has run successfully in the last N days."""
+# ruff: noqa: INP001
+
 import argparse
+import itertools
 import os
+import sys
+from datetime import datetime
+
+import requests
 
 # Constants
 GITHUB_TOKEN = os.environ["RAPIDS_GH_TOKEN"]
-GOOD_STATUSES = ("success",) # noqa
+GOOD_STATUSES = {"success"}
 
 
 def main(
@@ -15,14 +19,29 @@ def main(
     repo_owner: str,
     workflow_id: str,
     max_days_without_success: int,
-):
+    num_attempts: int = 5,
+) -> bool:
+    """Check whether a GHA workflow has run successfully in the last N days."""
     headers = {"Authorization": f"token {GITHUB_TOKEN}"}
     url = f"https://api.github.com/repos/{repo_owner}/{repo}/actions/workflows/{workflow_id}/runs"
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    exceptions = []
+    for _ in range(num_attempts):
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+            response.raise_for_status()
+            break
+        except requests.RequestException as e:
+            exceptions.append(e)
+    else:
+        msg = (
+            f"Failed to fetch {url} after {num_attempts} attempts with the following "
+            f"errors: \n\t{'\n\t'.join(exceptions)}"
+        )
+        raise RuntimeError(msg)
+
     runs = response.json()["workflow_runs"]
     tz = datetime.fromisoformat(runs[0]["run_started_at"]).tzinfo
-    NOW = datetime.now(tz=tz)
+    now = datetime.now(tz=tz)
 
     branch_ok = {}
     for branch, branch_runs in itertools.groupby(runs, key=lambda r: r["head_branch"]):
@@ -32,7 +51,7 @@ def main(
         branch_ok[branch] = False
         for run in sorted(branch_runs, key=lambda r: r["run_started_at"], reverse=True):
             if (
-                NOW - datetime.fromisoformat(run["run_started_at"])
+                now - datetime.fromisoformat(run["run_started_at"])
             ).days > max_days_without_success:
                 break
             if run["conclusion"] in GOOD_STATUSES:
@@ -41,10 +60,10 @@ def main(
 
     all_success = all(branch_ok.values())
     if not all_success:
-        print(
+        print(  # noqa: T201
             f"Branches with no successful runs of {workflow_id} in the last "
             f"{max_days_without_success} days: "
-            f"{', '.join(k for k, v in branch_ok.items() if not v)}"
+            f"{', '.join(k for k, v in branch_ok.items() if not v)}",
         )
     return not all(branch_ok.values())
 
@@ -53,7 +72,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("repo", type=str, help="Repository name")
     parser.add_argument(
-        "--repo-owner", default="rapidsai", help="Repository organziation/owner"
+        "--repo-owner",
+        default="rapidsai",
+        help="Repository organziation/owner",
     )
     parser.add_argument("--workflow-id", default="test.yaml", help="Workflow ID")
     parser.add_argument(
@@ -70,5 +91,5 @@ if __name__ == "__main__":
             args.repo_owner,
             args.workflow_id,
             args.max_days_without_success,
-        )
+        ),
     )

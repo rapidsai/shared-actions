9494b73ba601c975df21c4d40372beb0581bc814
Michael Sarahan <msarahan@nvidia.com>
2025-02-03 16:58:36 -0600

    refactor to simplify main func

--- telemetry-impls/summarize/send_trace.py
+++ telemetry-impls/summarize/send_trace.py
@@ -21,6 +21,7 @@ import logging
 import os
 from datetime import datetime, timezone
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 from opentelemetry import context, trace
 from opentelemetry.sdk.resources import Resource
@@ -29,6 +30,12 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.id_generator import IdGenerator
 from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.trace.status import StatusCode
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+    from typing import Any
+
+    from opentelemetry.context import Context
 
 match os.getenv("OTEL_EXPORTER_OTLP_PROTOCOL"):
     case "http/protobuf":
@@ -128,6 +135,98 @@ class RapidsSpanIdGenerator(IdGenerator):
         return self.trace_id
 
 
+def process_job_blob(  # noqa: PLR0913
+    trace_id: int,
+    ctx: Context,
+    job: Mapping[str, Any],
+    env_vars: Mapping[str, str],
+    first_timestamp: int,
+    last_timestamp: int,
+) -> int:
+    """Transform job JSON into an OTel span."""
+    # This is the top-level workflow, which we account for with the root
+    # trace above
+    if job["name"] == env_vars["OTEL_SERVICE_NAME"]:
+        return last_timestamp
+    # this cuts off matrix info from the job name, such that grafana can group
+    # these by name
+    if "/" in job["name"]:
+        job_name, matrix_part = job["name"].split("/", 1)
+    else:
+        job_name, matrix_part = job["name"], None
+
+    job_id = job["id"]
+    logging.info("Processing job: %s", job_name)
+    job_create = date_str_to_epoch(job["created_at"], first_timestamp)
+    job_start = date_str_to_epoch(job["started_at"], first_timestamp)
+    # this may get later in time as we progress through the steps. It is
+    # common to have the job completion time be earlier than the end of
+    # the final cleanup steps
+    job_last_timestamp = date_str_to_epoch(job["completed_at"], job_start)
+
+    if job_start == 0:
+        logging.info("Job is empty (no start time) - bypassing")
+        return last_timestamp
+
+    attribute_file = Path.cwd() / f"telemetry-artifacts/attrs-{job_id}"
+    attributes = {}
+    if attribute_file.exists():
+        logging.debug("Found attribute file for job: %s", job_id)
+        attributes = parse_attributes(attribute_file.as_posix())
+    else:
+        logging.debug("No attribute metadata found for job: %s", job_id)
+
+    attributes["service.name"] = job_name
+    job_provider = TracerProvider(
+        resource=Resource(attributes),
+        id_generator=RapidsSpanIdGenerator(trace_id=trace_id, job_name=job["name"]),
+    )
+    job_provider.add_span_processor(span_processor=SpanProcessor(OTLPSpanExporter()))
+    job_tracer = trace.get_tracer(
+        "GitHub Actions parser", "0.0.1", tracer_provider=job_provider
+    )
+
+    with job_tracer.start_as_current_span(
+        name=matrix_part or job["name"],
+        start_time=job_create,
+        context=ctx,
+        end_on_exit=False,
+    ) as job_span:
+        job_span.set_status(map_conclusion_to_status_code(job["conclusion"]))
+
+        job_provider.id_generator.update_step_name("Start delay time")
+        delay_span = job_tracer.start_span(
+            name="Start delay time", start_time=job_create
+        )
+        delay_span.end(job_start)
+
+        for step in job["steps"]:
+            start = date_str_to_epoch(step["started_at"], job_last_timestamp)
+            end = date_str_to_epoch(step["completed_at"], start)
+            job_provider.id_generator.update_step_name(step["name"])
+
+            if (end - start) / 1e9 > 1:
+                logging.info("processing step: %s", step["name"])
+                job_provider.id_generator.update_step_name(step["name"])
+                step_span = job_tracer.start_span(
+                    name=step["name"],
+                    start_time=start,
+                    attributes=attributes,
+                )
+                step_span.set_status(map_conclusion_to_status_code(step["conclusion"]))
+                step_span.end(end)
+
+                job_last_timestamp = max(end, job_last_timestamp)
+
+            job_end = max(
+                date_str_to_epoch(job["completed_at"], job_last_timestamp),
+                job_last_timestamp,
+            )
+            last_timestamp = max(job_end, last_timestamp)
+        job_span.end(job_end)
+    return last_timestamp
+
+
 def main() -> None:
     """Run core functionality."""
     with Path("all_jobs.json").open() as f:
@@ -169,92 +268,14 @@ def main() -> None:
         name="Top-level workflow root", start_time=first_timestamp, end_on_exit=False
     ) as root_span:
         for job in jobs:
-            # This is the top-level workflow, which we account for with the root
-            # trace above
-            if job["name"] == env_vars["OTEL_SERVICE_NAME"]:
-                continue
-            # this cuts off matrix info from the job name, such that grafana can group
-            # these by name
-            if "/" in job["name"]:
-                job_name, matrix_part = job["name"].split("/", 1)
-            else:
-                job_name, matrix_part = job["name"], None
-
-            job_id = job["id"]
-            logging.info("Processing job: %s", job_name)
-            job_create = date_str_to_epoch(job["created_at"], first_timestamp)
-            job_start = date_str_to_epoch(job["started_at"], first_timestamp)
-            # this may get later in time as we progress through the steps. It is
-            # common to have the job completion time be earlier than the end of
-            # the final cleanup steps
-            job_last_timestamp = date_str_to_epoch(job["completed_at"], job_start)
-
-            if job_start == 0:
-                logging.info("Job is empty (no start time) - bypassing")
-                continue
-
-            attribute_file = Path.cwd() / f"telemetry-artifacts/attrs-{job_id}"
-            attributes = {}
-            if attribute_file.exists():
-                logging.debug("Found attribute file for job: %s", job_id)
-                attributes = parse_attributes(attribute_file.as_posix())
-            else:
-                logging.debug("No attribute metadata found for job: %s", job_id)
-
-            attributes["service.name"] = job_name
-            job_provider = TracerProvider(
-                resource=Resource(attributes),
-                id_generator=RapidsSpanIdGenerator(
-                    trace_id=trace_id, job_name=job["name"]
-                ),
+            last_timestamp = process_job_blob(
+                trace_id=trace_id,
+                ctx=ctx,
+                job=job,
+                env_vars=env_vars,
+                first_timestamp=first_timestamp,
+                last_timestamp=last_timestamp,
             )
-            job_provider.add_span_processor(
-                span_processor=SpanProcessor(OTLPSpanExporter())
-            )
-            job_tracer = trace.get_tracer(
-                "GitHub Actions parser", "0.0.1", tracer_provider=job_provider
-            )
-
-            with job_tracer.start_as_current_span(
-                name=matrix_part or job["name"],
-                start_time=job_create,
-                context=ctx,
-                end_on_exit=False,
-            ) as job_span:
-                job_span.set_status(map_conclusion_to_status_code(job["conclusion"]))
-
-                job_provider.id_generator.update_step_name("Start delay time")
-                delay_span = job_tracer.start_span(
-                    name="Start delay time", start_time=job_create
-                )
-                delay_span.end(job_start)
-
-                for step in job["steps"]:
-                    start = date_str_to_epoch(step["started_at"], job_last_timestamp)
-                    end = date_str_to_epoch(step["completed_at"], start)
-                    job_provider.id_generator.update_step_name(step["name"])
-
-                    if (end - start) / 1e9 > 1:
-                        logging.info("processing step: %s", step["name"])
-                        job_provider.id_generator.update_step_name(step["name"])
-                        step_span = job_tracer.start_span(
-                            name=step["name"],
-                            start_time=start,
-                            attributes=attributes,
-                        )
-                        step_span.set_status(
-                            map_conclusion_to_status_code(step["conclusion"])
-                        )
-                        step_span.end(end)
-
-                        job_last_timestamp = max(end, job_last_timestamp)
-
-                    job_end = max(
-                        date_str_to_epoch(job["completed_at"], job_last_timestamp),
-                        job_last_timestamp,
-                    )
-                    last_timestamp = max(job_end, last_timestamp)
-                job_span.end(job_end)
         root_span.end(last_timestamp)
 
 

ac85bd816d5909013d03b5f8de7183b6c7795460
Mike Sarahan <msarahan@gmail.com>
2025-02-20 14:36:33 -0600

    Artifacts folder, change destination default, better python debug info (#44)
    * move to an artifact folder instead of one attr file
    
    * change to alloy instead of fluentbit. http -> grpc
    
    * python debug info; add parsing for sccache files
    
    * change branch refs to main

--- telemetry-impls/summarize/send_trace.py
+++ telemetry-impls/summarize/send_trace.py
@@ -19,23 +19,22 @@ import hashlib
 import json
 import logging
 import os
+import re
+from dataclasses import dataclass
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import TYPE_CHECKING
 
-from opentelemetry import context, trace
+from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.sdk.trace.id_generator import IdGenerator
-from opentelemetry.trace.propagation.tracecontext import TraceContextTextMapPropagator
 from opentelemetry.trace.status import StatusCode
 
 if TYPE_CHECKING:
     from collections.abc import Mapping
     from typing import Any
-
-    from opentelemetry.context import Context
 
 match os.getenv("OTEL_EXPORTER_OTLP_PROTOCOL"):
     case "http/protobuf":
@@ -52,7 +51,7 @@ match os.getenv("OTEL_EXPORTER_OTLP_PROTOCOL"):
         )
 
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.DEBUG)
 
 SpanProcessor = BatchSpanProcessor
 
@@ -71,6 +70,7 @@ def parse_attributes(attrs: os.PathLike | str | None) -> dict[str, str]:
     for attr in attrs_list:
         key, value = attr.split("=", 1)
         attributes[key] = value.strip().strip('"')
+        logging.debug("Attribute parsed: Key: %s, Value: %s", key, value)
     return attributes
 
 
@@ -109,10 +109,12 @@ class RapidsSpanIdGenerator(IdGenerator):
     def update_job_name(self, job_name: str) -> None:
         """Update the job name, which feeds into computing the span name."""
         self.job_name = job_name
+        logging.debug("Job name updated: %s", self.job_name)
 
     def update_step_name(self, step_name: str) -> None:
         """Update the step name, which feeds into computing the span name."""
         self.step_name = step_name
+        logging.debug("Step name updated: %s", self.step_name)
 
     def generate_span_id(self) -> int:
         """Get a new span ID.
@@ -132,9 +134,98 @@ class RapidsSpanIdGenerator(IdGenerator):
         return self.trace_id
 
 
+@dataclass
+class Compiler:
+    hits: int = 0
+    misses: int = 0
+    errors: int = 0
+
+    @property
+    def requests(self) -> int:
+        """Calculate the total requests."""
+        return self.hits + self.misses + self.errors
+
+    @property
+    def hit_rate(self) -> float:
+        """Calculate the cache hit rate."""
+        return self.hits / self.requests if self.requests > 0 else 0
+
+    @property
+    def miss_rate(self) -> float:
+        """Calculate the cache miss rate."""
+        return self.misses / self.requests if self.requests > 0 else 0
+
+    @property
+    def error_rate(self) -> float:
+        """Calculate the cache error rate."""
+        return self.errors / self.requests if self.requests > 0 else 0
+
+
+class SccacheStats:
+    requests: int = 0
+    compilers: dict[str, Compiler]
+
+    def __init__(self, requests: int, compilers: dict[str, Compiler]) -> None:
+        self.requests = requests
+        self.compilers = compilers
+
+    @property
+    def hits(self) -> int:
+        """Calculate the total cache hits."""
+        return sum(v.hits for v in self.compilers.values())
+
+    @property
+    def misses(self) -> int:
+        """Calculate the total cache misses."""
+        return sum(v.misses for v in self.compilers.values())
+
+    @property
+    def errors(self) -> int:
+        """Calculate the total cache errors."""
+        return sum(v.errors for v in self.compilers.values())
+
+    @property
+    def hit_rate(self) -> float:
+        """Calculate the total cache hit rate."""
+        return self.hits / self.requests if self.requests > 0 else 0
+
+    @property
+    def miss_rate(self) -> float:
+        """Calculate the total cache miss rate."""
+        return self.misses / self.requests if self.requests > 0 else 0
+
+    @property
+    def error_rate(self) -> float:
+        """Calculate the total cache error rate."""
+        return self.errors / self.requests if self.requests > 0 else 0
+
+
+def get_sccache_stats(artifact_folder: Path) -> dict[str, str]:
+    """Get sccache stats from the artifact folder."""
+    stats_files = artifact_folder.glob("sccache-stats*.txt")
+    logging.debug("SCCache stats files: %s", stats_files)
+    parsed_stats = {}
+    lang_line_match = re.compile(r"Cache (?P<result>\w+) \((?P<lang>\w+)[^)]*\)\s*(?P<count>\d+)")
+    for file in stats_files:
+        with file.open() as f:
+            stats = SccacheStats(requests=0, compilers={"c": Compiler(), "cpp": Compiler(), "cuda": Compiler()})
+            for line in f:
+                if match := re.match(r"^Compile\srequests\s+(\d+).*", line, re.IGNORECASE):
+                    stats.requests = int(match.group(1))
+                elif match := lang_line_match.match(line):
+                    compiler = stats.compilers[match.group("lang")]
+                    setattr(compiler, match.group("result"), int(match.group("count")))
+        stats_file_name = re.findall(r"sccache-stats[-]?(?P<name>\w+).txt", file.name)
+        if stats_file_name:
+            stats_file_name = stats_file_name[0]
+        else:
+            stats_file_name = "main_process"
+        parsed_stats[stats_file_name] = stats
+    return parsed_stats
+
+
 def process_job_blob(  # noqa: PLR0913
     trace_id: int,
-    ctx: Context,
     job: Mapping[str, Any],
     env_vars: Mapping[str, str],
     first_timestamp: int,
@@ -144,11 +235,14 @@ def process_job_blob(  # noqa: PLR0913
     # This is the top-level workflow, which we account for with the root
     # trace above
     if job["name"] == env_vars["OTEL_SERVICE_NAME"]:
+        logging.debug("Job name is the same as the service name: %s", job["name"])
         return last_timestamp
     # this cuts off matrix info from the job name, such that grafana can group
     # these by name
     if "/" in job["name"]:
         job_name, matrix_part = job["name"].split("/", 1)
+        job_name = job_name.strip()
+        matrix_part = matrix_part.strip()
     else:
         job_name, matrix_part = job["name"], None
 
@@ -165,15 +259,18 @@ def process_job_blob(  # noqa: PLR0913
         logging.info("Job is empty (no start time) - bypassing")
         return last_timestamp
 
-    attribute_file = Path.cwd() / f"telemetry-artifacts/attrs-{job_id}"
+    artifact_folder = Path.cwd() / f"telemetry-artifacts/telemetry-tools-artifacts-{job_id}"
     attributes = {}
-    if attribute_file.exists():
+    if (artifact_folder / "attrs").exists():
         logging.debug("Found attribute file for job: %s", job_id)
-        attributes = parse_attributes(attribute_file.as_posix())
+        attributes = parse_attributes(artifact_folder / "attrs")
     else:
         logging.debug("No attribute metadata found for job: %s", job_id)
 
     attributes["service.name"] = job_name
+
+    sccache_stats = get_sccache_stats(artifact_folder)
+
     job_provider = TracerProvider(
         resource=Resource(attributes),
         id_generator=RapidsSpanIdGenerator(trace_id=trace_id, job_name=job["name"]),
@@ -184,33 +281,59 @@ def process_job_blob(  # noqa: PLR0913
     with job_tracer.start_as_current_span(
         name=matrix_part or job["name"],
         start_time=job_create,
-        context=ctx,
         end_on_exit=False,
     ) as job_span:
+        logging.debug("Job span created: %s", job_span)
         job_span.set_status(map_conclusion_to_status_code(job["conclusion"]))
 
         job_provider.id_generator.update_step_name("Start delay time")
         delay_span = job_tracer.start_span(name="Start delay time", start_time=job_create)
         delay_span.end(job_start)
+        logging.debug("Delay span created: %s", delay_span)
 
         for step in job["steps"]:
-            start = date_str_to_epoch(step["started_at"], job_last_timestamp)
-            end = date_str_to_epoch(step["completed_at"], start)
-            job_last_timestamp = max(end, job_last_timestamp)
+            step_start = date_str_to_epoch(step["started_at"], job_last_timestamp)
+            step_end = date_str_to_epoch(step["completed_at"], step_start)
+            job_last_timestamp = max(step_end, job_last_timestamp)
             job_provider.id_generator.update_step_name(step["name"])
+            # Reset attributes for each step
+            span_attributes = {}
 
-            if (end - start) / 1e9 > 1:
-                logging.info("processing step: %s", step["name"])
+            if (step_end - step_start) / 1e9 > 1:
+                logging.debug("processing step: %s", step["name"])
                 job_provider.id_generator.update_step_name(step["name"])
+                # Only add sccache attributes if this is a build step
+                if re.match(r"(?:[\w+]+\sbuild$)|(?:Build\sand\srepair.*)", step["name"], re.IGNORECASE):
+                    logging.debug("Adding sccache attributes for step: %s", step["name"])
+                    for file_name, stats in sccache_stats.items():
+                        span_attributes[f"sccache.{file_name}.hit_rate"] = stats.hit_rate
+                        span_attributes[f"sccache.{file_name}.miss_rate"] = stats.miss_rate
+                        span_attributes[f"sccache.{file_name}.error_rate"] = stats.error_rate
+                        span_attributes[f"sccache.{file_name}.requests"] = stats.requests
+                        for lang, lang_stats in stats.compilers.items():
+                            span_attributes[f"sccache.{file_name}.{lang}.hit_rate"] = lang_stats.hit_rate
+                            span_attributes[f"sccache.{file_name}.{lang}.miss_rate"] = lang_stats.miss_rate
+                            span_attributes[f"sccache.{file_name}.{lang}.error_rate"] = lang_stats.error_rate
+                            span_attributes[f"sccache.{file_name}.{lang}.requests"] = lang_stats.requests
+
+                # TODO: Ninja log files?
+                # TODO: file sizes of packages or their contents
+
                 step_span = job_tracer.start_span(
                     name=step["name"],
-                    start_time=start,
-                    attributes=attributes,
+                    start_time=step_start,
+                    attributes=span_attributes,
                 )
+                logging.debug("Step span created: %s", step_span)
+                # TODO: use step_span.record_exception(exc) to capture errors. exc is an exception object,
+                # so if we are reading from a log file, we need to read the log file into an exception object.
                 step_span.set_status(map_conclusion_to_status_code(step["conclusion"]))
-                step_span.end(end)
-        last_timestamp = max(job_last_timestamp, last_timestamp)
+                step_span.end(step_end)
+                logging.debug("Step span ended: %s", step_span)
+            else:
+                logging.debug("Skipping step: %s, because its duration is < 1s", step["name"])
         job_span.end(job_last_timestamp)
+        logging.debug("Job span ended: %s", job_span)
     return last_timestamp
 
 
@@ -219,25 +342,22 @@ def main() -> None:
     with Path("all_jobs.json").open() as f:
         jobs = json.loads(f.read())
 
-    env_vars = parse_attributes("telemetry-artifacts/telemetry-env-vars")
-
     first_timestamp = date_str_to_epoch(jobs[0]["created_at"])
     # track the latest timestamp observed and use it for any unavailable times.
     last_timestamp = date_str_to_epoch(jobs[0]["completed_at"], 0)
 
-    attribute_files = list(Path.cwd().glob("telemetry-artifacts/attrs-*"))
-    if attribute_files:
-        attribute_file = attribute_files[0]
+    attribute_folders = list(Path.cwd().glob("telemetry-artifacts/telemetry-tools-artifacts-*"))
+    logging.debug("Attribute folders: %s", attribute_folders)
+    if attribute_folders:
+        attribute_file = attribute_folders[0] / "attrs"
         attributes = parse_attributes(attribute_file.as_posix())
+        env_vars = parse_attributes(attribute_folders[0] / "telemetry-env-vars")
+        logging.debug("Env vars parsed from first attribute folder: %s", env_vars)
     else:
         attributes = {}
     global_attrs = {k: v for k, v in attributes.items() if k.startswith("git.")}
     global_attrs["service.name"] = env_vars["OTEL_SERVICE_NAME"]
 
-    ctx = TraceContextTextMapPropagator().extract(
-        carrier={"traceparent": env_vars["TRACEPARENT"]},
-    )
-    context.attach(ctx)
     trace_id = int(env_vars["TRACEPARENT"].split("-")[1], 16)
 
     provider = TracerProvider(
@@ -250,16 +370,17 @@ def main() -> None:
     with tracer.start_as_current_span(
         name="Top-level workflow root", start_time=first_timestamp, end_on_exit=False
     ) as root_span:
+        logging.debug("Root span created: %s", root_span)
         for job in jobs:
             last_timestamp = process_job_blob(
                 trace_id=trace_id,
-                ctx=ctx,
                 job=job,
                 env_vars=env_vars,
                 first_timestamp=first_timestamp,
                 last_timestamp=last_timestamp,
             )
         root_span.end(last_timestamp)
+        logging.debug("Root span ended: %s", root_span)
 
 
 if __name__ == "__main__":
